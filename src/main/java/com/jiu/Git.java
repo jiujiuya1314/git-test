698ed945e8b8e909960ac8cc075274f68e48e8ab
xyj <13790879017@163.com>
2022-06-14 17:22:06 +0800

    hot-fix merge commit2.

--- src/main/java/com/jiu/Git.java
+++ src/main/java/com/jiu/Git.java
@@ -13,6 +13,8 @@ public class Git {
         System.out.println("hello git.");
         System.out.println("hello git.");
         System.out.println("hot-fix-merge");
+        System.out.println("hot-fix test1");
+        System.out.println("hot-fix test2");
     }
 
 }
